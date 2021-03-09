d5af9babf0a671c00a774526941882a4c08e3721
Hampus Toft <hamtof-0@student.ltu.se>
2021-03-09 15:23:44 +0100

    added some methods to help with the usage of checkout object inside of Events.

--- src/lab5/state/supermarket/Checkout.java
+++ src/lab5/state/supermarket/Checkout.java
@@ -1,5 +1,7 @@
 package lab5.state.supermarket;
 
+import com.sun.istack.internal.NotNull;
+
 public class Checkout {
 
     private int checkoutTotal;
@@ -12,19 +14,67 @@ public class Checkout {
         this.checkoutQueue = new FIFO();
     }
 
-    public boolean isEmpty(){
-        return checkoutsOccupied <= 0;
-    }
-
+    /**
+     * Used to find if there is a empty checkout or not
+     * @return true if empty, false otherwise
+     */
     public boolean hasEmptyCheckout(){
         return checkoutsOccupied < checkoutTotal;
     }
 
-    public void add(Customer customer){
+    /**
+     * Used to find if the checkoutQueue is empty or not
+     * @return true if empty, false otherwise
+     */
+    public boolean queueIsEmpty(){
+        return checkoutQueue.isEmpty();
+    }
+
+    /**
+     * add a customer to the queue
+     * @param customer the customer that should be placed in the checkout queue
+     */
+    public void addToQueue(@NotNull Customer customer){
         checkoutQueue.add(customer);
     }
 
-    public void remove(){
+    /**
+     * Used if at anypoint it is important to know what customer is getting served
+     * @param customer the customer object getting served
+     */
+    public void serveCustomer(@NotNull Customer customer){
+        serveCustomer(); // TODO: Change if actually needed
+    }
+
+    /**
+     * Used if we don't track what customer is getting served.
+     */
+    public void serveCustomer(){
+        if(!this.hasEmptyCheckout()) throw new RuntimeException("No empty checkouts to use!");
+        if(!this.queueIsEmpty()) this.remove(); // someone is in the queue serve the first
+        // Occupied a checkout
+        checkoutsOccupied ++;
+    }
+
+    /**
+     * Used if at anypoint it is important to know what customer was served
+     * @param customer the customer object getting served
+     */
+    public void customerServed(@NotNull Customer customer){
+        customerServed(); // TODO: Change if actually needed
+    }
+
+    /**
+     * Used if we don't track what customer was served.
+     */
+    public void customerServed(){
+        checkoutsOccupied --;
+    }
+
+    /**
+     * Remove the first person from the queue
+     */
+    private void remove(){
         checkoutQueue.removeFirst();
     }
 }
