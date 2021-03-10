197ee789a6aaccae6f2324addcf43f86cde5fdbf
Hampus Toft <hamtof-0@student.ltu.se>
2021-03-10 10:08:22 +0100

    Updated them to use the new name method from Event also added the super.execute to run Event's execute method...

--- src/lab5/events/StopEvent.java
+++ src/lab5/events/StopEvent.java
@@ -11,12 +11,12 @@ import lab5.state.SimState;
 public class StopEvent extends Event {
 
     public StopEvent(EventQueue eventQueue, SimState state, double executeTime){
-    	super(eventQueue, state, executeTime);
+    	super(eventQueue, state, executeTime, "Stop");
     }
 
     @Override
     public void execute() {
-        //TODO: Add code to stop simulation
+        super.execute();
         state.stop();// Using the handbrake in State to stop
     }
 }
